d4087a48876a22f71a3b9ba801a638a15504a49c
SolarHaddock46 <vladimir@matsnev.ru>
2024-02-13 13:28:07 +0300

    tested localisable strings and edited schemes

--- SSL/SSL/ViewController.swift
+++ SSL/SSL/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+//    private lazy  var testLabel: UILabel = {
+//        let label = UILabel()
+//        label.text = NSLocalizedString("Sign up", comment: "test")
+//        return label
+//    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+//        view.addSubview(testLabel)
+//        testLabel.translatesAutoresizingMaskIntoConstraints = false
+//        testLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+//        testLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+
     }
-
-
 }
-

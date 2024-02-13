87a1b493c4aad9f4a1a3d6e1f4ee1fb19073830d
SolarHaddock46 <vladimir@matsnev.ru>
2024-02-13 21:16:35 +0300

    attempted to connect a font
    it has no russian though so this might need to be redone later

--- SSL/SSL/ViewController.swift
+++ SSL/SSL/ViewController.swift
@@ -1,22 +1,38 @@
-//
-//  ViewController.swift
-//  SSL
-//
-//  Created by Владимир Мацнев on 13.02.2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
-    private lazy var textField = UserInfoTextField(placeholder: NSLocalizedString("Password", comment: "a"), isSecure: false)
+    
+    private lazy var testView: UIStackView = {
+        let view = UIStackView()
+        view.spacing = 16
+        view.axis = .vertical
+        return view
+    }()
+    
+    private lazy var testLabel = CustomLabel(localisationKey: "Log in")
+    
+    private lazy var textField = UserInfoTextField(placeholder: NSLocalizedString("Password", comment: "a"), isSecure: true)
+    
+    private lazy var emailField = UserInfoTextField(placeholder: NSLocalizedString("Email", comment: "a"), isSecure: false)
+    
+    private lazy var testButton = PrimaryButton(localizationKey: "Sign up")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        view.addSubview(textField)
-        textField.translatesAutoresizingMaskIntoConstraints = false
-        textField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        textField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
-        textField.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        testView.addArrangedSubview(testLabel)
+        testView.addArrangedSubview(textField)
+        testView.addArrangedSubview(emailField)
+        testView.addArrangedSubview(testButton)
+        
+        view.addSubview(testView)
+        testView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            testView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            testView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            testView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16)
+        ])
     }
 }

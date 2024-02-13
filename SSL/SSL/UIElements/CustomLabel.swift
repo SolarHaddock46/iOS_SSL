48e900364de2f67d1d122fbbc487b1c007c7cffb
SolarHaddock46 <vladimir@matsnev.ru>
2024-02-13 14:49:41 +0300

    separated CustomLabel into its own class

---
+++ SSL/SSL/UIElements/CustomLabel.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+class CustomLabel: UILabel {
+    init(localisationKey: String) {
+        super.init(frame: .zero)
+        setup(localisationKey: localisationKey)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setup(localisationKey: String) {
+        self.text = NSLocalizedString(localisationKey, comment: "comment")
+        self.textColor = .white
+        if let customFont = UIFont(name: "Onest", size: 16) {
+            self.font = customFont
+        } else {
+            self.font = UIFont.systemFont(ofSize: 16)
+        }
+    }
+}
+
--- SSL/SSL/UIElements/PrimaryButton.swift
+++ SSL/SSL/UIElements/PrimaryButton.swift
@@ -2,23 +2,12 @@ import UIKit
 
 final class PrimaryButton: UIButton {
 
-    internal lazy var customTitleLabel: (String) -> UILabel = { [unowned self] localisationKey in
-        let label = UILabel()
-        label.text = NSLocalizedString(localisationKey, comment: "comment")
-        label.textColor = .white
-        if let customFont = UIFont(name: "Onest", size: 16) {
-            label.font = customFont
-        } else {
-            label.font = UIFont.systemFont(ofSize: 16)
-        }
-        return label
-    }
     init(localizationKey: String) {
         super.init(frame: .zero)
         backgroundColor = UIColor(red: 63/255, green: 162/255, blue: 254/255, alpha: 1.0)
         layer.cornerRadius = 16
         heightAnchor.constraint(equalToConstant: 48).isActive = true
-        let titleLabel = customTitleLabel(localizationKey)
+        let titleLabel = CustomLabel(localisationKey: localizationKey)
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
         // Add custom label to the button
         addSubview(titleLabel)

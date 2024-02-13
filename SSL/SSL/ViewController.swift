33df3964777853d7ce6aae2c65896c097b7d8337
SolarHaddock46 <vladimir@matsnev.ru>
2024-02-13 12:38:15 +0300

    created project and deleted storyboard

---
+++ SSL/SSL/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SSL
+//
+//  Created by Владимир Мацнев on 13.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

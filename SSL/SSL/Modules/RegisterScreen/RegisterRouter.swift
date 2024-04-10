6479450b08a797837ee3ae0a61834aaa9332715a
Мацнев Владимир Дмитриевич <vdmatsnev@edu.hse.ru>
2024-04-10 15:47:01 +0300

    register router

--- SSL/SSL/Modules/RegisterScreen/RegisterRouter.swift
+++ SSL/SSL/Modules/RegisterScreen/RegisterRouter.swift
@@ -1,8 +1,20 @@
-//
-//  RegisterRouter.swift
-//  SSL
-//
-//  Created by Владимир Мацнев on 10.04.2024.
-//
-
 import Foundation
+import UIKit
+
+class RegisterRouter: RegisterPresenterToRouterProtocol {
+    static func createModule() -> UIViewController {
+        let view = RegisterFirstViewController()
+        let presenter = RegisterPresenter()
+        let interactor = RegisterInteractor()
+        let router = RegisterRouter()
+
+        view.presenter = presenter
+        presenter.view = view
+        presenter.router = router
+        presenter.interactor = interactor
+        interactor.presenter = presenter
+
+        return view
+    }
+}
+

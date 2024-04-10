7e9a5f89e8ecc5f5163cc60661fd1b72627db1eb
Мацнев Владимир Дмитриевич <vdmatsnev@edu.hse.ru>
2024-04-10 15:38:31 +0300

    register protocols

--- SSL/SSL/Modules/RegisterScreen/RegisterProtocols.swift
+++ SSL/SSL/Modules/RegisterScreen/RegisterProtocols.swift
@@ -1,8 +1,30 @@
-//
-//  RegisterProtocols.swift
-//  SSL
-//
-//  Created by Владимир Мацнев on 10.04.2024.
-//
-
 import Foundation
+import UIKit
+
+protocol RegisterInteractorToPresenterProtocol {
+    func registerSuccess(with response: UserRegisterDTO)
+    func registerFailed(with error: NetworkError)
+}
+
+protocol RegisterPresenterToInteractorProtocol {
+    var presenter: RegisterInteractorToPresenterProtocol? { get set }
+    func performRegister(with user: RegisterRequestDTO) async throws
+}
+
+protocol RegisterViewToPresenterProtocol {
+    var view: RegisterPresenterToViewProtocol? { get set }
+    var interactor: RegisterPresenterToInteractorProtocol? { get set }
+    var router: RegisterPresenterToRouterProtocol? { get set }
+    func startRegister(firstName: String, secondName: String, fatherName: String?, telegram: String, email: String, password1: String, password2: String, hsePass: Bool, acceptConditions: Bool) async throws
+}
+
+protocol RegisterPresenterToViewProtocol {
+    var presenter: RegisterViewToPresenterProtocol? { get set }
+    func showAlert(title: String, message: String)
+    func hideLoading()
+}
+
+protocol RegisterPresenterToRouterProtocol {
+    static func createModule() -> UIViewController
+}
+
